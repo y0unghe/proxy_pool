48c3e04b0734c2fed5ed53283cfe067809d5cf07
yong <yong.dev@outlook.com>
2020-05-09 23:31:50 +0800

    Make it work

--- Config/setting.py
+++ Config/setting.py
@@ -33,9 +33,9 @@ HEADER = """
 
 PY3 = sys.version_info >= (3,)
 
-DB_TYPE = getenv('db_type', 'SSDB').upper()
+DB_TYPE = getenv('db_type', 'REDIS').upper()
 DB_HOST = getenv('db_host', '127.0.0.1')
-DB_PORT = getenv('db_port', 8888)
+DB_PORT = getenv('db_port', 6379)
 DB_PASSWORD = getenv('db_password', '')
 
 
