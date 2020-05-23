15c1d31e38a03a1132cb3a33136bbe029254a672
yong <yong.dev@outlook.com>
2020-05-23 16:29:35 +0800

    Change url

--- Util/utilFunction.py
+++ Util/utilFunction.py
@@ -86,7 +86,7 @@ def validUsefulProxy(proxy):
         proxy = proxy.decode("utf8")
     proxies = {"http": "http://{proxy}".format(proxy=proxy)}
     try:
-        r = requests.get('https://www.asklib.com/', proxies=proxies, timeout=10, verify=False)
+        r = requests.get('https://www.asklib.com/view/ab5e90dd.html', proxies=proxies, timeout=10, verify=False)
         if r.status_code == 200:
             return True
     except Exception as e:
